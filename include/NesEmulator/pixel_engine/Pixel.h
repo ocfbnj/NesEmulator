6a91cfa76e6784f534f695e0857ee161086646fa
ocfbnj <ocfbnj@qq.com>
2021-12-07 13:17:05 +0800

    add system palette

--- include/NesEmulator/pixel_engine/Pixel.h
+++ include/NesEmulator/pixel_engine/Pixel.h
@@ -2,6 +2,7 @@
 #define NESEMULATOR_PIXEL_H
 
 #include <cstdint>
+#include <array>
 
 struct Pixel {
     uint8_t r;
@@ -9,5 +10,7 @@ struct Pixel {
     uint8_t b;
     uint8_t a;
 };
+
+extern std::array<Pixel, 64> SystemPalette;
 
 #endif // NESEMULATOR_PIXEL_H
