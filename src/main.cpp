5b903967a6c810a42498d4c13438998541afd88f
ocfbnj <ocfbnj@qq.com>
2021-12-09 14:15:21 +0800

    parse command line parameters

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,13 @@
+#include <iostream>
+
 #include "Emulator.h"
 
-int main() {
-    Emulator emulator{"nestest.nes"};
+int main(int argc, char* argv[]) {
+    if (argc != 2) {
+        std::cerr << "Usage: " << argv[0] << " <nes file>\n";
+        return -1;
+    }
+
+    Emulator emulator{argv[1]};
     emulator.run();
 }
