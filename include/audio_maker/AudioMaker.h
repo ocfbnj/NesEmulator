07b1d89000c034911fd7fa23465652f60349ccbf
ocfbnj <ocfbnj@qq.com>
2021-12-28 13:12:07 +0800

    create directories for `ocfbnj::audio_maker`

---
+++ include/audio_maker/AudioMaker.h
@@ -0,0 +1,9 @@
+#ifndef OCFBNJ_AUDIO_MAKER_H
+#define OCFBNJ_AUDIO_MAKER_H
+
+class AudioMaker {
+public:
+private:
+};
+
+#endif // OCFBNJ_AUDIO_MAKER_H
---
+++ src/audio_maker/AudioMaker.cpp
@@ -0,0 +1,1 @@
+#include <audio_maker/AudioMaker.h>
